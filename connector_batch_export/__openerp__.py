2f3b93092f8cd3bff015cba6857cec7465579844
Sébastien BEAU <sebastien.beau@akretion.com>
2016-03-02 17:48:07 +0100

    [ADD] add connector_batch_export, this module is an abstract module that help for synchronising system by batch

---
+++ connector_batch_export/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{'name': 'Connector Batch Export',
+ 'version': '0.0.1',
+ 'author': 'Akretion',
+ 'website': 'www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': [
+     'connector',
+ ],
+ 'data': [
+ ],
+ 'installable': True,
+ 'application': True,
+}
---
+++ connector_batch_export/models/batch_binding.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class BatchBinding(models.AbstractModel):
+    _name= "batch.binding"
+
+    sync_state = fields.Selection([
+        ('to_update', 'To update'),
+        ('ok', 'OK'),
+        ], default='to_update',
+        readonly=True)
+    date_modified = fields.Date(readonly=True)
+    date_syncronized = fields.Date(readonly=True)
