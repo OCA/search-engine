36c59f1f773f8ad4578af940c4a3ece78383c4ff
Sébastien BEAU <sebastien.beau@akretion.com>
2018-07-30 17:22:48 +0200

    [REF] start to refactor the export, by splitting it in two step. One for computing the json (in multi worker), one for reading and pushing the data. The export is not more resilient (not blocked for one record that fail), faster because json can be computed in multi-worker

--- connector_search_engine/models/se_index.py
+++ connector_search_engine/models/se_index.py
@@ -2,7 +2,8 @@
 # Copyright 2013 Akretion (http://www.akretion.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.addons.queue_job.job import job
 
 
 class SeIndex(models.Model):
@@ -26,6 +27,9 @@ class SeIndex(models.Model):
     exporter_id = fields.Many2one(
         'ir.exports',
         string='Exporter')
+    batch_size = fields.Integer(
+        default=5000,
+        help='Batch size for exporting element')
 
     _sql_constraints = [
         ('lang_model_uniq', 'unique(backend_id, lang_id, model_id)',
@@ -33,17 +37,46 @@ class SeIndex(models.Model):
     ]
 
     @api.model
-    def export_all_index(self, domain=None, delay=True):
+    def recompute_all_index(self, domain=None):
         if domain is None:
             domain = []
-        return self.search(domain).export_all(delay=delay)
+        return self.search(domain).recompute_all_binding()
 
-    @api.multi
-    def export_all(self, delay=True):
+    def recompute_all_binding(self):
         for record in self:
             binding_obj = self.env[record.model_id.model]
-            bindings = binding_obj.search([('index_id', '=', record.id)])
-            bindings.write({'sync_state': 'to_update'})
-            binding_obj._scheduler_export(
-                domain=[('index_id', '=', record.id)], delay=delay)
+            for bindings in binding_obj.search([('index_id', '=', record.id)]):
+                bindings._jobify_recompute_json()
+        return True
+
+    @api.model
+    def generate_batch_export_per_index(self, domain=None):
+        if domain is None:
+            domain = []
+        for record in self.search(domain):
+            description = _('Prepare a batch export of indexes')
+            record.with_delay(description=description).prepare_batch_export()
+        return True
+
+    def _get_domain_for_exporting_binding(self):
+        return [('index_id', '=', self.id), ('sync_state', '=', 'to_update')]
+
+    @job(default_channel='root.search_engine.prepare_batch_export')
+    def prepare_batch_export(self):
+        self.ensure_one()
+        domain = self._get_domain_for_exporting_binding()
+        bindings = self.env[self.model_id.model].search(domain)
+        bindings_count = len(bindings)
+        while bindings:
+            todo = processing = bindings[0:self.batch_size]
+            bindings = bindings[self.batch_size:]
+            description = _(
+                "Export %d records of %d for index '%s'") % (
+                    len(processing),
+                    bindings_count,
+                    self.name)
+            processing.with_delay(description=description).export()
+            processing.with_context(connector_no_export=True).write({
+                'sync_state': 'scheduled',
+            })
         return True
