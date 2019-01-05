b4b35b29bb5a33c6accc1a64a86f42c848e2f4e2
Simone Orsi <simahawk@gmail.com>
2019-01-05 10:01:20 +0100

    connector_search_engine: drop outdated win action

--- connector_search_engine/models/se_index.py
+++ connector_search_engine/models/se_index.py
@@ -38,20 +38,6 @@ class SeIndex(models.Model):
     ]
 
     @api.multi
-    def action_synchronize(self):
-        view = self.env.ref(
-            'connector_search_engine.se_index_synchronize_form_view')
-        return {
-            'name': _('Synchronize %s') % self.name,
-            'view_type': 'form',
-            'target': 'new',
-            'res_model': self._name,
-            'res_id': self.id,
-            'views': [(view.id, 'form')],
-            'type': 'ir.actions.act_window',
-        }
-
-    @api.multi
     def recompute_all_index(self, domain=None):
         recordset = self
         if domain is not None:
