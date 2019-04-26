aa424c04c9eff3390622e0cb0199eccf162e75f7
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-04-26 17:47:25 +0200

    [FIX] connector_elasticsearch: Makes the config on the index required only for elasticsearch. This changes allows the use of algolia backend at same time as elasticsearch

--- connector_elasticsearch/models/se_index.py
+++ connector_elasticsearch/models/se_index.py
@@ -2,7 +2,8 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SeIndex(models.Model):
@@ -15,5 +16,15 @@ class SeIndex(models.Model):
         help="Elasticseacrh index definition (see https://www.elastic.co/"
         "guide/en/elasticsearch/reference/current/"
         "indices-create-index.html)",
-        required=True,
     )
+
+    @api.constrains("config_id", "backend_id")
+    def _check_config_id_required(self):
+        for rec in self:
+            if (
+                rec.backend_id.specific_model == "se.backend.elasticsearch"
+                and not rec.config_id
+            ):
+                raise ValidationError(
+                    _("An index definition is rquired for elasticsearch")
+                )
---
+++ connector_elasticsearch/readme/HISTORY.rst
@@ -0,0 +1,6 @@
+10.0.?.?.? (unreleased)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* connector_elasticsearch: Makes the config on index required only if the
+  index is linked to an elastisearch backend. This change allows the usage
+  of algolia search engine at the same time as elasticsearch.
