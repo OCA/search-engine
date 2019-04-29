08db058c33d507e20b89f20563c39710f1880702
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-04-29 15:26:35 +0200

    [FIX] setup.py

--- setup/connector_elasticsearch/setup.py
+++ setup/connector_elasticsearch/setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addons={
+    odoo_addon={
         'external_dependencies_override': {
             'python': {
                 'elasticsearch': 'elasticsearch>=6.0.0,<7.0.0',
