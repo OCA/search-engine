ad28f0987e4e45713e147297982919c3129aa161
hparfr <raphael.reverdy@akretion.com>
2021-12-06 16:11:04 +0100

    Fix missing config_id from views
    There is no reasons to hide config_id  in connector_search_engine
    
    And the help message in connector_elasticsearch overflow in other
    implementations

--- connector_elasticsearch/models/se_index.py
+++ connector_elasticsearch/models/se_index.py
@@ -1,19 +1,13 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class SeIndex(models.Model):
 
     _inherit = "se.index"
-
-    config_id = fields.Many2one(
-        help="ElasticSearch index definition (see https://www.elastic.co/"
-        "guide/en/elasticsearch/reference/current/"
-        "indices-create-index.html)",
-    )
 
     @api.constrains("config_id", "backend_id")
     def _check_config_id_required(self):
