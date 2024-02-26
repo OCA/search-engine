81f8e3cd7ad97ba6b5ff7d3453c88ca4173500e2
Simone Orsi <simahawk@gmail.com>
2024-02-26 11:32:15 +0100

    connector_search_engine: allow recompute specific bindings
    _jobify_batch_recompute and batch_recompute now accept a binding_ids param
    
    to force the records to be computed.

--- connector_search_engine/wizards/se_binding_state_updater.py
+++ connector_search_engine/wizards/se_binding_state_updater.py
@@ -1,7 +1,9 @@
 # Copyright 2023 ACSONE SA/NV
+# Copyright 2024 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.tools import groupby
 
 
 class SeBindingStateUpdater(models.TransientModel):
@@ -14,7 +16,12 @@ class SeBindingStateUpdater(models.TransientModel):
         selection=lambda self: self.env["se.binding"]._fields["state"].selection,
         required=True,
     )
+    do_it_now = fields.Boolean(help="Don't wait for the cron to process these records")
 
     def doit(self):
         res_ids = self.env.context.get("active_ids")
-        self.env["se.binding"].browse(res_ids).write({"state": self.state})
+        selected_bindings = self.env["se.binding"].browse(res_ids)
+        selected_bindings.write({"state": self.state})
+        if self.do_it_now and self.state == "to_recompute":
+            for index, bindings in groupby(selected_bindings, key=lambda x: x.index_id):
+                index._jobify_batch_recompute(binding_ids=[x.id for x in bindings])
